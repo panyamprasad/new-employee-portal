949e7493a32eeb6d2ad8ee3f22e9f3c712ca8a47
Prasad <37583509+PrasadPanyam@users.noreply.github.com>
2023-10-05 23:23:44 +0530

    Moving the latest changes

--- api.js
+++ api.js
@@ -1,10 +1,116 @@
-import { DynamoDBClient} from "@aws-sdk/client-dynamodb";
-import {
-    DynamoDBDocumentClient,
-    PutCommand,
-    GetCommand,
-    ScanCommand,
-    DeleteCommand,
-    UpdateCommand,
-  } from "@aws-sdk/lib-dynamodb";
-  
+const { DynamoDB } = require('aws-sdk');
+const dynamoDb = new DynamoDB.DocumentClient();
+
+// Create Experience Details
+module.exports.createExperience = async (event) => {
+  const requestBody = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE_NAME,
+    Item: {
+      EmpId: requestBody.EmpId,
+      CompanyName: requestBody.CompanyName,
+      CompanyLocation: requestBody.CompanyLocation,
+      StartDate: requestBody.StartDate,
+      EndDate: requestBody.EndDate,
+      PerformedRole: requestBody.PerformedRole,
+      Responsibilities: requestBody.Responsibilities,
+      TechnologiesWorked: requestBody.TechnologiesWorked,
+      IsActive: requestBody.IsActive,
+    },
+  };
+
+  try {
+    await dynamoDb.put(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Experience details created successfully' }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'An error occurred while creating experience details' }),
+    };
+  }
+};
+
+// Update Experience Details
+module.exports.updateExperience = async (event) => {
+  const employeeId = event.pathParameters.employeeId;
+  const requestBody = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE_NAME,
+    Key: { EmpId: employeeId },
+    UpdateExpression:
+      'SET CompanyName = :companyName, CompanyLocation = :companyLocation, StartDate = :startDate, EndDate = :endDate, ' +
+      'PerformedRole = :performedRole, Responsibilities = :responsibilities, TechnologiesWorked = :technologiesWorked, IsActive = :isActive',
+    ExpressionAttributeValues: {
+      ':companyName': requestBody.CompanyName,
+      ':companyLocation': requestBody.CompanyLocation,
+      ':startDate': requestBody.StartDate,
+      ':endDate': requestBody.EndDate,
+      ':performedRole': requestBody.PerformedRole,
+      ':responsibilities': requestBody.Responsibilities,
+      ':technologiesWorked': requestBody.TechnologiesWorked,
+      ':isActive': requestBody.IsActive,
+    },
+  };
+
+  try {
+    await dynamoDb.update(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Experience details updated successfully' }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'An error occurred while updating experience details' }),
+    };
+  }
+};
+
+// Get Experience Details
+// module.exports.getExperience = async (event) => {
+//   const employeeId = event.pathParameters.employeeId;
+
+//   const params = {
+//     TableName: process.env.DYNAMODB_TABLE_NAME,
+//     Key: { EmpId: employeeId },
+//   };
+
+//   try {
+//     const result = await dynamoDb.get(params).promise();
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(result.Item),
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'An error occurred while getting experience details' }),
+//     };
+//   }
+// };
+
+// Get All Experience Details
+// module.exports.getAllExperience = async () => {
+//   const params = {
+//     TableName: process.env.DYNAMODB_TABLE_NAME,
+//   };
+
+//   try {
+//     const result = await dynamoDb.scan(params).promise();
+//     const items = result.Items.map((item) => DynamoDB.Converter.unmarshall(item));
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(items),
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'An error occurred while fetching all experience details' }),
+//     };
+//   }
+// };
