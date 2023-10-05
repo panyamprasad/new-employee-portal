08d9e4fbb930183f8b49b416e61a796240d82b69
Prasad <37583509+PrasadPanyam@users.noreply.github.com>
2023-10-05 22:13:43 +0530

    Done the UI changes.

---
+++ api.js
@@ -0,0 +1,10 @@
+import { DynamoDBClient} from "@aws-sdk/client-dynamodb";
+import {
+    DynamoDBDocumentClient,
+    PutCommand,
+    GetCommand,
+    ScanCommand,
+    DeleteCommand,
+    UpdateCommand,
+  } from "@aws-sdk/lib-dynamodb";
+  
